92db7fd3aa708b2b88635e5385b9f4ab78a6d971
sohatach <sohatach+git@gmail.com>
2014-06-11 00:12:59 +0900

    イベント参加ページにイベント一覧を表示するようにした。

--- src/main/scala/controller/JoinController.scala
+++ src/main/scala/controller/JoinController.scala
@@ -2,10 +2,24 @@ package controller
 
 import skinny._
 import skinny.validator._
+import model.Event
 
 class JoinController extends ApplicationController {
   protectFromForgery()
 
-  def index = render("/join/index")
+  protected def enablePagination = true
 
+  def index = {
+    if (enablePagination) {
+      val pageNo: Int = params.getAs[Int]("page").getOrElse(1)
+      val pageSize: Int = 3
+      val totalCount: Long = Event.count()
+      val totalPages: Int = (totalCount / pageSize).toInt + (if (totalCount % pageSize == 0) 0 else 1)
+      set("items", Event.findAllWithPagination(Pagination.page(pageNo).per(pageSize)))
+      set("totalPages", totalPages)
+    } else {
+      set("items", Event.findAll())
+    }
+    render(s"/join/index")
+  }
 }
